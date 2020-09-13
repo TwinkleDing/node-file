8242efd653d0591efefc070a954121bca48d00ef
TinkleDing <twinkle415@163.com>
2020-09-13 15:52:16 +0800

    add build

--- src/app.js
+++ src/app.js
@@ -6,21 +6,25 @@ const bodyparser = require('koa-bodyparser');
 const cors = require('koa2-cors');
 const json = require('koa-json');
 const router = require('./router');
+const args = require('minimist')(process.argv.slice(2));
 
 const app = new Koa();
 app.use(cors())
 app.use(json())
-app.use(logger())
 app.use(router.routes());
 app.use(bodyparser({
   enableTypes: ['json', 'form', 'text']
 }))
-app.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+
+if(!args.build) {
+  app.use(logger())
+  app.use(async (ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  })
+}
 
 const port = '3333';
 const server = http.createServer(app.callback());
